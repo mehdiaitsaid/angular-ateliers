183817cb06cd3f9b41846a6c7b13ab2791ee23e2
mehdiaitsaid <aitsaid.mahdi@gmail.com>
2022-06-21 00:51:01 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  public taches: String[] = [];
+  public tache: String = '';
+
+  ajouterTache(){
+    if (this.tache === ''){
+       alert('Veuillez remplir le champ');
+       return;
+      }
+    
+    this.taches.push(this.tache)
+    this.tache = ''
+  }
+
+
+
 }
